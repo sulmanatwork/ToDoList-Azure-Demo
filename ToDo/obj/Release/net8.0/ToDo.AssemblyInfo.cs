ff1612c64bdd3a27545efd0846df73740c751ed5
sulmanatwork <sulmanatwork@gmail.com>
2024-11-22 16:05:21 +0500

    by sulman

--- ToDo/obj/Release/net8.0/ToDo.AssemblyInfo.cs
+++ ToDo/obj/Release/net8.0/ToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-ToDo-a58d9825-12c6-4537-b061-47fb08eb1f24")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8bec31c67d25b5b513841cae3d12f689a93fc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
